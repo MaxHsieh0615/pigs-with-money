dd68500ff2a982d225ff5517736651b8870f32d4
Joshua Moore <jommoore2003@me.com>
2019-02-24 16:26:42 -0800

    Creating job form

---
+++ client/src/components/CreateJob/createjob.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import DeleteBtn from "../DeleteBtn";
+import Jumbotron from "../Jumbotron";
+import API from "../../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../Grid";
+import { List, ListItem } from "../List";
+import { Input, TextArea, FormBtn } from "../Form";
+
+class CreateJob extends Component {
+  state = {
+    jobs: [],
+    title: "",
+    description: "",
+    budget: 0,
+    status: "",
+  };
+
+//   componentDidMount() {
+//     this.loadJobs();
+//   }
+
+//   loadJobs = () => {
+//     API.getJobs()
+//       .then(res =>
+//         this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+//       )
+//       .catch(err => console.log(err));
+//   };
+
+//   deleteJob = id => {
+//     API.deleteJob(id)
+//       .then(res => this.loadJobs())
+//       .catch(err => console.log(err));
+//   };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.title && this.state.description) {
+      API.createJob({
+        title: this.state.title,
+        description: this.state.description,
+        budget: this.state.budget
+      })
+        // .then(res => this.loadJobs())
+        .catch(err => console.log(err));
+    }
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            <Jumbotron>
+              <h1>Create Job</h1>
+            </Jumbotron>
+            <form>
+              <Input
+                type="text"
+                value={this.state.title}
+                onChange={this.handleInputChange}
+                name="title"
+                placeholder="Title (required)"
+              />
+              <TextArea
+                value={this.state.description}
+                onChange={this.handleInputChange}
+                name="description"
+                placeholder="Description (required)"
+              />
+              <Input
+                type="number"
+                value={this.state.budget}
+                onChange={this.handleInputChange}
+                name="budget"
+                placeholder="Budget (Optional)"
+              />
+              <FormBtn
+                disabled={!(this.state.description && this.state.title)}
+                onClick={this.handleFormSubmit}
+              >
+                Submit Job
+              </FormBtn>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default CreateJob;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,16 +3,12 @@ import axios from "axios";
 export default {
   // Gets all books
   //View tasks
-  getBooks: function() {
-    return axios.get("/api/books");
-  },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+  getCreateJob: function() {
+    return axios.post("/api/createjobs");
   },
   // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+  deleteCreateJob: function(id) {
+    return axios.delete("/api/createjobs/" + id);
   },
   //complete tasks router
   completeTask:{}, 
@@ -20,7 +16,7 @@ export default {
 
   // Saves a book to the database
   //Submit task
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+  saveCreateJob: function(jobData) {
+    return axios.post("/api/createjobs", jobData);
   }
 };
