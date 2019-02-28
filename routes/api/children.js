b95eb3d0f9a63f319f7bb4a9bbcca07488a090e0
Dennis Tam <dennist82@Denniss-MacBook-Pro.local>
2019-02-27 20:08:50 -0800

    fix for express children routes.

--- routes/api/children.js
+++ routes/api/children.js
@@ -1,18 +1,8 @@
-const router = require("express").Router();
 const childrenController = require('../../controllers/childrenController');
 
-// module.exports = (app) =>{
-//     app.post('/api/AddChild',childrenController.createChild);
-// }
-
-
-
 module.exports = (app) =>{
-    app.post('/api/AddChild',childrenController.createChild);
-    // FIXME: need to fix proper post route
-    app.post('/api/addchild',childrenController.createChild);
+    app.post('/api/addChild',childrenController.createChild);
     app.get("/api/findAllByChild", childrenController.findAllByChild);
-    app.get("api/findbyid", childrenController.findById);
-    app.put("/api/updatechildbudget", childrenController.updateChildBudget);
-    app.delete("/api/removechild", childrenController.removeChild);
+    app.put("/api/updateChildBudget", childrenController.updateChildBudget);
+    app.delete("/api/removeChild", childrenController.removeChild);
 }
