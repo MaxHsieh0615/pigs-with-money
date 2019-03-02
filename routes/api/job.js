93b1f3e794e29ad1762fa29de3a40d4dc3cb5b13
MaxH <MaxH@Maxs-MacBook-Pro.local>
2019-03-02 13:45:24 -0800

    here

--- routes/api/job.js
+++ routes/api/job.js
@@ -2,11 +2,11 @@
 const jobsController = require('../../controllers/jobsController');
 
 module.exports = (app) =>{
-  app.post('/api/CreateJob',jobsController.createJob);
+  app.post('/api/createJob',jobsController.createJob);
   app.get("/api/findAllByRequestor", jobsController.findAllByRequestor);
   app.get("/api/findAllByAssignedTo", jobsController.findAllByAssignedTo);
-  app.get("api/findbyid", jobsController.findById);
-  app.put("/api/updatejob", jobsController.update);
-  app.put("/api/completejob", jobsController.complete);
-  app.delete("/api/removejob", jobsController.remove);
+  app.get("api/findById", jobsController.findById);
+  app.put("/api/upDateJob", jobsController.update);
+  app.put("/api/completeJob", jobsController.complete);
+  app.delete("/api/removeJob", jobsController.remove);
 }
