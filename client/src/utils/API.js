2a1adf0303012c545d47756d28ad3aa53ad91759
Dennis Tam <dennist82@Denniss-MacBook-Pro.local>
2019-03-02 14:10:22 -0800

    fix route issue.

---
+++ client/src/utils/API.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+export default {
+  // Gets all books
+  //View tasks
+  getCreateJob: function(jobData) {
+    return axios.post("/api/createjob", jobData);
+  },
+  // Deletes the book with the given id
+  deleteCreateJob: function(id) {
+    return axios.delete("/api/createjobs/" + id);
+  },
+
+  //Submit task
+  saveCreateJob: function(jobData) {
+    return axios.post("/api/createjobs", jobData);
+  },
+
+  // TODO CHILD & CHILDREN Section
+
+  getAddChild: function(childrenData) {
+    return axios.post("/api/addchild", childrenData);
+  },
+
+  deleteAddChild: function(id) {
+    return axios.delete("/api/addchild/" + id);
+  },
+  // Add Child
+  saveAddChild: function(childrenData) {
+    return axios.post("/api/addchild", childrenData);
+  },
+
+  getAllJobs: function (){
+    return axios.get("/api/getAllJobs");
+  },
+  //complete tasks router
+  completeTask:{}, 
+  updateTask:{},
+
+};
