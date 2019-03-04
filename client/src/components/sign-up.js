4299f6cff31006052ab25b9aecb652d27e95d2c8
Dennis Tam <dennist82@Denniss-MacBook-Pro.local>
2019-03-03 22:26:01 -0800

    sign up will redirect back to login page.  getJobs will filter by current user as requestorEmail.

--- client/src/components/sign-up.js
+++ client/src/components/sign-up.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import axios from 'axios'
 
 class Signup extends Component {
@@ -44,55 +45,58 @@ class Signup extends Component {
 
 
 	render() {
-	  const {flashMessage} = this.state;
-	  return (
-		<div className="SignupForm">
-		  <h4>Sign up</h4>
-		  <form className="form-horizontal">
-		    <div className="form-group">
-		      <div className="col-1 col-ml-auto">
-			    <label className="form-label" htmlFor="email">Email</label>
-		      </div>
-		      <div className="col-3 col-mr-auto">
-			    <input className="form-input"
-			      type="text"
-			      id="email"
-			      name="email"
-			      placeholder="email"
-			      value={this.state.email}
-			      onChange={this.handleChange}
-			    />
-		      </div>
-		    </div>
-			<div className="form-group">
-			  <div className="col-1 col-ml-auto">
-				<label className="form-label" htmlFor="password">Password: </label>
-			  </div>
-			  <div className="col-3 col-mr-auto">
-				<input className="form-input"
-			  	  placeholder="password"
-				  type="password"
-				  name="password"
-				  value={this.state.password}
-				  onChange={this.handleChange}
-				/>
-			  </div>
-			</div>
-			{flashMessage.length>0 && <h5>{flashMessage}</h5>}
-			<div className="form-group ">
-			  <div className="col-7"></div>
-			  <button
-				className="btn btn-primary col-1 col-mr-auto"
-				onClick={this.handleSubmit}
-				type="submit"
-			  >Sign up
-			  </button>
-		    </div>
-		  </form>
-	    </div>
-
-	  )
-  }
+		const {flashMessage} = this.state;
+		if(this.state.redirectTo){
+			return <Redirect to={{ pathname: this.state.redirectTo }} />
+		}else{
+			return (
+			<div className="SignupForm">
+				<h4>Sign up</h4>
+				<form className="form-horizontal">
+					<div className="form-group">
+						<div className="col-1 col-ml-auto">
+						<label className="form-label" htmlFor="email">Email</label>
+						</div>
+						<div className="col-3 col-mr-auto">
+						<input className="form-input"
+							type="text"
+							id="email"
+							name="email"
+							placeholder="email"
+							value={this.state.email}
+							onChange={this.handleChange}
+						/>
+						</div>
+					</div>
+				<div className="form-group">
+					<div className="col-1 col-ml-auto">
+					<label className="form-label" htmlFor="password">Password: </label>
+					</div>
+					<div className="col-3 col-mr-auto">
+					<input className="form-input"
+							placeholder="password"
+						type="password"
+						name="password"
+						value={this.state.password}
+						onChange={this.handleChange}
+					/>
+					</div>
+				</div>
+				{flashMessage.length>0 && <h5>{flashMessage}</h5>}
+				<div className="form-group ">
+					<div className="col-7"></div>
+					<button
+					className="btn btn-primary col-1 col-mr-auto"
+					onClick={this.handleSubmit}
+					type="submit"
+					>Sign up
+					</button>
+					</div>
+				</form>
+				</div>
+			)
+		}
+	}
 }
 
 export default Signup
