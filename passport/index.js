d0a7d069cf495183b8a57871ad8471a7fbf3a258
Dennis Tam <dennist82@Denniss-MacBook-Pro.local>
2019-03-04 22:30:25 -0800

    fix the remainder of the Darren's comments.

--- passport/index.js
+++ passport/index.js
@@ -4,19 +4,13 @@ const db = require('../models')
 
 // called on login, saves the id to session req.session.passport.user = {id:'..'}
 passport.serializeUser((user, done) => {
-  console.log('*** serializeUser called, user: ')
-  console.log('---------')
   done(null, { _id: user._id })
 })
 
 // user object attaches to the request as req.user
 passport.deserializeUser((id, done) => {
-  console.log('DeserializeUser called')
-  console.log(id);
   db.Users.findOne(
 	{where: id }).then(user => {
-	  console.log(`*** Deserialize user, user` )
-	  console.log('--------------')
 	  done(null, user);
 	})
 });
