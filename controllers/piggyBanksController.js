c88d011d922b0164433bc8855783f7a45fad8474
richardluong127 <richardluong127@gmail.com>
2019-03-05 23:09:26 -0800

    cleaned up piggycontrollers

--- controllers/piggyBanksController.js
+++ controllers/piggyBanksController.js
@@ -1,30 +1,5 @@
 const db = require("../models");
 
-// Defining methods for the piggyBanksController
-// module.exports = {
-//   findPiggyBankByChild: function(req, res) {
-//     db.PiggyBank.findOne({ where: { child_name: req.body.child_name } })
-//       .then(dbModel => res.json(dbModel))
-//       .catch(err => res.status(422).json(err));
-//   },
-//   findPiggyBankByPiggyBankID: function(req, res) {
-//     db.PiggyBank.findOne({ where: { piggy_bank_id: req.body.piggy_bank_id } })
-//       .then(dbModel => res.json(dbModel))
-//       .catch(err => res.status(422).json(err));
-//   },
-//   updatePiggyBankBalance: function(req, res) {
-//     db.PiggyBank.findOneAndUpdate({ _id: req.params.id }, req.body)
-//       .then(dbModel => res.json(dbModel))
-//       .catch(err => res.status(422).json(err));
-//   }
-// // };
-// app.get("/api/addFunds/:id", piggyBanksController.addFunds);
-
-// app.get("/api/deductFunds/:id", piggyBanksController.deductFunds);
-
-// app.get("/api/showAllTransactions/:id", piggyBanksController.showAllTransactions);
-
-
 module.exports = {
   addFunds: function(req, res) {
     db.Job.findById({where: {id: req.params.id}})
