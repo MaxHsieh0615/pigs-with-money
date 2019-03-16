449950a88bb23191e6ca2e71b743c148c8b2dcaa
Joshua Moore <jommoore2003@me.com>
2019-03-16 11:55:15 -0700

    made recommended change from Darren

--- controllers/piggyBanksController.js
+++ controllers/piggyBanksController.js
@@ -11,7 +11,7 @@ module.exports = {
         description : `Completed: ${job.id} ${job.title}`
       };
       db.PiggyBank.create(dataSet)})
-      .then(transaction => console.log("Deposite money Successfully."))
+      .then(transaction => console.log("Deposit money Successfully."))
       .catch(err => console.log(err));
   },
   deductFunds: function(productId,childId) {
